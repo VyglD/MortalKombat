aa6bb31f32f6cc5bffc6081ed634a66dc37faffb
VyglD <VyglD@yandex.ru>
2021-10-14 09:39:37 +0300

    homework-2

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
-console.log( 'init' );
+const createNode = ( template ) => {
+    const wrapper = document.createElement( 'div' );
+    wrapper.innerHTML = template;
+
+    return wrapper.firstElementChild;
+};
+
+const createPlayerObject = ( name, hp, img, weapon ) => {
+    return {
+        name,
+        hp,
+        img,
+        weapon,
+        attack() {
+            console.log( `${this.name} Fight...` );
+        }
+    }
+};
+
+const createPlayer = ( playerClass, playerObject ) => {
+    const template = (`
+        <div class="${playerClass}">
+            <div class="progressbar">
+                <div class="life" style="width: ${playerObject.hp}%"></div>
+                <div class="name">${playerObject.name}</div>
+            </div>
+            <div class="character">
+                <img src="${playerObject.img}" />
+            </div>
+        </div>
+    `);
+
+    return createNode( template );
+};
+
+const player1 = createPlayerObject(
+    'scorpion',
+    50,
+    'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+    ['gun']
+);
+const player2 = createPlayerObject(
+    'kitana',
+    80,
+    'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+    ['knife']
+);
+
+const $playerNode1 = createPlayer( 'player2', player1 );
+const $playerNode2 = createPlayer( 'player2', player2 );
+
+const $arenas = document.querySelector( 'div.arenas' );
+
+$arenas.appendChild( $playerNode1 );
+$arenas.appendChild( $playerNode2 );
+
+
